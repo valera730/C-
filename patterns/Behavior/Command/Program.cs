623b07e7a2658d21b621a2e804230714e0cb481e
Kirill Lypkan <lypkankirill@gmail.com>
2020-09-08 13:57:33 +0300

    added command pattern

---
+++ patterns/Behavior/Command/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pult pult = new Pult();
+        TV tv = new TV();
+        pult.SetCommand(new TVOnCommand(tv));
+        pult.PressButton();
+        pult.PressUndo();
+
+        Console.Read();
+    }
+}
+
+interface ICommand
+{
+    void Execute();
+    void Undo();
+}
+
+// Receiver
+class TV
+{
+    public void On()
+    {
+        Console.WriteLine("TV is on!");
+    }
+
+    public void Off()
+    {
+        Console.WriteLine("TV is off...");
+    }
+}
+
+class TVOnCommand : ICommand
+{
+    TV tv;
+    public TVOnCommand(TV tvSet)
+    {
+        tv = tvSet;
+    }
+    public void Execute()
+    {
+        tv.On();
+    }
+    public void Undo()
+    {
+        tv.Off();
+    }
+}
+
+// Invoker
+class Pult
+{
+    ICommand command;
+
+    public Pult() { }
+
+    public void SetCommand(ICommand com)
+    {
+        command = com;
+    }
+
+    public void PressButton()
+    {
+        command.Execute();
+    }
+    public void PressUndo()
+    {
+        command.Undo();
+    }
+}

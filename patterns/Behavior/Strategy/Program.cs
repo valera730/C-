60e4ebf48b2ad7f01d601f8bfeb75adb8d73eeaa
Kirill Lypkan <lypkankirill@gmail.com>
2020-09-08 10:54:00 +0300

    added strategy pattern

---
+++ patterns/Behavior/Strategy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car auto = new Car(4, "Volvo", new PetrolMove());
+        auto.Move();
+        auto.Movable = new ElectricMove();
+        auto.Move();
+
+        Console.ReadLine();
+    }
+}
+interface IMovable
+{
+    void Move();
+}
+
+class PetrolMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Moving on gasoline");
+    }
+}
+
+class ElectricMove : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Moving on electricity");
+    }
+}
+class Car
+{
+    protected int passengers;
+    protected string model;
+
+    public Car(int num, string model, IMovable mov)
+    {
+        this.passengers = num;
+        this.model = model;
+        Movable = mov;
+    }
+    public IMovable Movable { private get; set; }
+    public void Move()
+    {
+        Movable.Move();
+    }
+}

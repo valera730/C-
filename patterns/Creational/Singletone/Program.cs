64d2da1576de88a704759dfbe3d06af16edecf5f
Kirill Lypkan <lypkankirill@gmail.com>
2020-09-08 09:42:25 +0300

    added singletone pattern

---
+++ patterns/Creational/Singletone/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}

498099457b021e0ecce620974cae6b1303dbde01
Kirill Lypkan <lypkankirill@gmail.com>
2020-09-08 14:04:11 +0300

    added iterator pattern

---
+++ patterns/Behavior/Iterator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Library library = new Library();
+        Reader reader = new Reader();
+        reader.SeeBooks(library);
+
+        Console.Read();
+    }
+}
+
+class Reader
+{
+    public void SeeBooks(Library library)
+    {
+        IBookIterator iterator = library.CreateNumerator();
+        while (iterator.HasNext())
+        {
+            Book book = iterator.Next();
+            Console.WriteLine(book.Name);
+        }
+    }
+}
+
+interface IBookIterator
+{
+    bool HasNext();
+    Book Next();
+}
+interface IBookNumerable
+{
+    IBookIterator CreateNumerator();
+    int Count { get; }
+    Book this[int index] { get; }
+}
+class Book
+{
+    public string Name { get; set; }
+}
+
+class Library : IBookNumerable
+{
+    private Book[] books;
+    public Library()
+    {
+        books = new Book[]
+        {
+            new Book{Name="First book"},
+            new Book {Name="Second book"},
+            new Book {Name="Third book"}
+        };
+    }
+    public int Count
+    {
+        get { return books.Length; }
+    }
+
+    public Book this[int index]
+    {
+        get { return books[index]; }
+    }
+    public IBookIterator CreateNumerator()
+    {
+        return new LibraryNumerator(this);
+    }
+}
+class LibraryNumerator : IBookIterator
+{
+    IBookNumerable aggregate;
+    int index = 0;
+    public LibraryNumerator(IBookNumerable a)
+    {
+        aggregate = a;
+    }
+    public bool HasNext()
+    {
+        return index < aggregate.Count;
+    }
+
+    public Book Next()
+    {
+        return aggregate[index++];
+    }
+}

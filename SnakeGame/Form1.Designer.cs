e33dc5d9db22c8fc45b17bcae27a1df0e3d706dc
valera730 <telmob@yandex.ru>
2020-09-12 17:52:07 +0300

    SnakeGame

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Snake
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(530, 450);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.Timer timer3;
+		private System.Windows.Forms.Timer timer4;
+		private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+    }
+}
+
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+	public partial class Form1 : Form
+	{
+		Point[] p;
+		Point apple;
+		int len;
+		int direction;	//1-left; 2-right; 3-top; 4-down
+		public Form1()
+		{
+			InitializeComponent();
+			len = 5;
+			p = new Point[200];
+			direction = 3;
+			for (int i = 0; i < 5; i++)
+			{
+				p[i].X = 50;
+				p[i].Y = 50 + i*10;
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+			for(int i=198;i>=0;i--)
+            {
+				p[i+1].X = p[i].X;
+				p[i+1].Y = p[i].Y;
+            }
+			if(direction == 1)
+            {
+				p[0].X = p[1].X - 10;
+				p[0].Y = p[1].Y;
+            }
+			if (direction == 2)
+			{
+				p[0].X = p[1].X + 10;
+				p[0].Y = p[1].Y;
+			}
+			if (direction == 3)
+			{
+				p[0].X = p[1].X;
+				p[0].Y = p[1].Y - 10;
+			}
+			if (direction == 4)
+			{
+				p[0].X = p[1].X;
+				p[0].Y = p[1].Y + 10;
+			}
+			SolidBrush b = new SolidBrush(Color.Brown);
+			for(int i=0;i<len;i++)
+            {
+				e.Graphics.FillEllipse(b,p[i].X, p[i].Y,10,10);
+            }
+			SolidBrush b1 = new SolidBrush(Color.Green);
+			e.Graphics.FillEllipse(b1,apple.X, apple.Y, 10,10);
+			if(p[0].X==apple.X & p[0].Y==apple.Y)
+            {
+				len++;
+				Random R;
+				R = new Random();
+				apple.X = R.Next(0,20)*10;
+				apple.Y = R.Next(0,20)*10;
+				timer1.Interval--;
+            }
+
+		}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+			panel1.Invalidate();	//перерисовка формы
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+			if (e.KeyCode == Keys.Left)
+				direction = 1;
+			if (e.KeyCode == Keys.Right)
+				direction = 2;
+			if (e.KeyCode == Keys.Up)
+				direction = 3;
+			if (e.KeyCode == Keys.Down)
+				direction = 4;
+		}
+    }
+}
